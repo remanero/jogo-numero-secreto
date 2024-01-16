dd60248afe99a7c57d86ad993a326c6cb3bdd0f3
remanero <remanero@gmail.com>
2024-01-15 23:50:37 -0300

    Adicionando a pasta app

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,26 @@
+const elementoChute = document.getElementById('chute');
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br';
+recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e) {
+    //console.log(e.results[0][0].transcript);
+    var varChute = e.results[0][0].transcript;
+    //console.log(varChute);
+    exibeChuteNaTela(varChute);
+    verificaChute(varChute);
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+    <div>Você disse:</div>
+    <span class="box">${chute}</span>
+    `
+}
+
+recognition.addEventListener('end', () => recognition.start())
